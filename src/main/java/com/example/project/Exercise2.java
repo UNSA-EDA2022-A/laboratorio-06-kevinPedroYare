385cf61bb30946c2f9debe220608ebeb0efee552
kevinPedroYare <83080715+kevinPedroYare@users.noreply.github.com>
2022-07-11 14:40:41 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -20,8 +20,29 @@ public class Exercise2 {
     }
 
 
-    public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2){
+        public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2) {
+        if (a1.isEmpty() && a2.isEmpty())
+            return true;
+        else if (a1.isEmpty() || a2.isEmpty())
+            return false;
+        else {
+            if (a1.root.data.compareTo(a2.root.data) == 0)
+                return bstSimilares(a1.root.left, a2.root.left) && bstSimilares(a1.root.right, a2.root.right);
+            else
+                return false;
+        }
+    }
 
-        return false;
+    private <T extends Comparable<T>> boolean bstSimilares(Node<T> nodo, Node<T> nodo2) {
+        if (nodo == null && nodo2 == null)
+            return true;
+        else if (nodo == null || nodo2 == null)
+            return false;
+        else {
+            if (nodo.data.compareTo(nodo2.data) == 0)
+                return bstSimilares(nodo.left, nodo2.left) && bstSimilares(nodo.right, nodo2.right);
+            else
+                return false;
+        }
     }
 }
